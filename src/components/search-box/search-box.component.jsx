b2089e9735b59de5050ad67534e5691a4b0b47f6
john edwards <joedw1978@gmail.com>
2023-03-01 20:28:52 -0500

    all components converted to functions

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,20 +1,15 @@
-import { Component } from "react";
 import "./search-box.styles.css";
 
-export class SearchBox extends Component {
-  render() {
-    const {onChangeHandler, className, placeHolder} = this.props;
-
-    return (
-      <div>
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <input
-          className={`search-box ${className}`}
-          placeholder={placeHolder}
-          type="search"
-          onChange={onChangeHandler}
-        />
-      </div>
-    );
-  }
-}
+export const SearchBox = ({ onChangeHandler, className, placeHolder }) => {
+  return (
+    <div>
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <input
+        className={`search-box ${className}`}
+        placeholder={placeHolder}
+        type="search"
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
+};
